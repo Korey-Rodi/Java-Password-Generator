c45b6bed49ca25f362c45095b49d24b70a5d20f7
Korey Rodi <rodikorey@gmail.com>
2025-12-19 01:46:32 -0500

    Intro lines

---
+++ password.java
@@ -0,0 +1,6 @@
+public class password {
+    public static void main(String[] args) {
+        System.out.println("=======================================");
+        System.out.println("Welcome to the java password generator!");
+    }
+}
